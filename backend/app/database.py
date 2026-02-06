798d89625afc3ac40a7a883c5f3fe8d9c93709c1
Harikrishnan R <harishtherock2001@gmail.com>
2026-02-06 16:47:39 +0530

    Use SQLite for deployment (remove psycopg2 dependency)

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,10 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql+psycopg2://postgres:postgres123@localhost:5432/financial_health"
+DATABASE_URL = "sqlite:///./financial.db"
 
-engine = create_engine(DATABASE_URL)
 
+#DATABASE_URL = "postgresql+psycopg2://postgres:postgres123@localhost:5432/financial_health"
+
+engine = create_engine ( DATABASE_URL,
+    connect_args={"check_same_thread": False}
+)
 SessionLocal = sessionmaker(
     autocommit=False,
     autoflush=False,
